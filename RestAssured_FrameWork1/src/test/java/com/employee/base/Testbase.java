a9f3cc7df798306c6430bbcb94f008a1032769d1
santhosh94 <santhosh.raman87@gmail.com>
2022-04-23 17:59:52 +0530

    adding demo proect

--- RestAssured_FrameWork1/src/test/java/com/employee/base/Testbase.java
+++ RestAssured_FrameWork1/src/test/java/com/employee/base/Testbase.java
@@ -2,11 +2,12 @@ package com.employee.base;
 
 import org.testng.annotations.BeforeTest;
 
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class Testbase {
-
+	
 	public static RequestSpecification httprequest;
 	public static Response response;
 	
---
+++ RestAssured_FrameWork1/src/test/java/com/employee/testcases/TC_02_Get_SingleUSer.java
@@ -0,0 +1,62 @@
+package com.employee.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.employee.base.Testbase;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+
+public class TC_02_Get_SingleUSer extends Testbase {
+
+	@BeforeClass
+	void getAllEmployees() throws InterruptedException {
+		
+		RestAssured.baseURI="https://reqres.in/";
+	
+		httprequest=RestAssured.given();
+		// request response
+		response=httprequest.request(Method.GET,"api/users?page=2");
+		
+		Thread.sleep(3000);
+	}
+	
+	@Test
+	void checkResponseBody() {
+		print("checking response body");
+		String responsebody=response.getBody().asString();
+		System.out.println("Body is:"+responsebody);
+		Assert.assertTrue(responsebody!=null);
+	}
+	@Test
+	void checkStatusCode() {
+		print("checkStatusCode");
+		int StatusCode=response.getStatusCode();
+		System.out.println("StatusCode is:"+StatusCode);
+	}
+	@Test
+	void checkResponsetime() {
+		print("checkResponsetime");
+		long Responsetime=response.getTime();
+		System.out.println("Responsetime is:"+Responsetime);
+		if(Responsetime>2000)
+			print("response time is longer than 20000");
+		Assert.assertTrue(Responsetime<2000);
+		
+	}
+	@Test
+	void checkContentType() {
+		print("checkContentType");
+		String contenttype=response.header("Content-Type");
+		System.out.println("content type : "+contenttype);
+		Assert.assertEquals(contenttype, "application/json; charset=utf-8");
+	}
+	
+	@AfterClass
+	void teardown() {
+		print("Test case 01 completed.");
+	}
+}
